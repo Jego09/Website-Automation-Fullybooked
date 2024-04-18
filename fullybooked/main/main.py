b8edb8a0a050c95b8603e186df64a00edebb1bb2
jego09 <jegolimpiado@gmail.com>
2024-04-18 09:22:02 +0800

    Update main.py

--- fullybooked/main/main.py
+++ fullybooked/main/main.py
@@ -1,13 +1,11 @@
 import unittest
 from search_open_product import *
 from open_pages import *
-from login import *
 from wishlist_no_acc import *
 from checkout_complete import *
 from contact_us import *
 from store_locator import *
 from admin import *
-from LoginCustomer import *
 from selenium.webdriver.chrome.options import Options
 from elements import Login
 from dotenv import load_dotenv
